d34e909d73d78cd72edc76f6e04907ba1a8f6e9c
tanahara <s10.vermouth.4869@gmail.com>
2022-03-30 22:28:39 +0900

    一覧表示を実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.order(created_at: :desc)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    render :new, status: :unprocessable_entity unless @user.save
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    render :edit, status: :unprocessable_entity unless @user.update(user_params)
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :gender, :age, :tel, :birth_date)
+  end
+end
